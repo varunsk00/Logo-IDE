b50e114fb769c12716b3734a16d59931a83f7199
Maverick Chung <mc608@duke.edu>
2020-02-18 17:53:08 -0500

    begun command

---
+++ src/compiler/Command.java
@@ -0,0 +1,28 @@
+package compiler;
+
+import java.util.ArrayList;
+
+abstract class Command {
+
+  public int desiredArgs;
+  protected ArrayList<Command> args;
+
+  public Command(){
+    args = new ArrayList<>();
+  }
+
+  abstract double execute();
+
+  public void addArg(Command arg) {
+    args.add(arg);
+  }
+
+  public void addArg(Command arg, int n) {
+    while(args.size()<n) {
+      args.add(null);
+    }
+    args.set(n, arg);
+  }
+
+
+}
