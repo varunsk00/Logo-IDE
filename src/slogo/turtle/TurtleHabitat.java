5407d1dd80e6452c1b67b25ad9c8e3a51c31f2c7
Varun Kosgi <vsk10@duke.edu>
2020-02-26 00:48:06 -0500

    added multiple colors in one line

--- src/slogo/turtle/TurtleHabitat.java
+++ src/slogo/turtle/TurtleHabitat.java
@@ -5,10 +5,13 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Polyline;
 import javafx.scene.shape.Rectangle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TurtleHabitat {
     private Pane myTurtleHabitat;
     private TurtleView turtle;
-    private Polyline pen  = new Polyline();
+    private List myLines;
 
     public TurtleHabitat(double width, double height){
         turtle = new TurtleView(50, 25);
@@ -16,8 +19,8 @@ public class TurtleHabitat {
         turtle.setX(width/2);
         turtle.setY(height/2);
         myTurtleHabitat = new Pane(turtle);
+        myLines = new ArrayList<Polyline>();
         changeSize(width, height);
-        myTurtleHabitat.getChildren().add(pen);
     }
 
     public void changeSize(double width, double height){
@@ -37,14 +40,20 @@ public class TurtleHabitat {
     }
 
     public void penDraw(Color penColor, double x_coor, double y_coor){
+        Polyline pen  = new Polyline();
+        myLines.add(pen);
         Double[] points = new Double[] {turtle.centerX(), turtle.centerY(),
                                         x_coor + turtle.getXOffset() + 50/(2), y_coor + turtle.getYOffset() + 25/(2)};
         pen.getPoints().addAll(points);
-        //p.getStrokeDashArray().addAll(2d, 21d);
         pen.setStroke(penColor);
+        //p.getStrokeDashArray().addAll(2d, 21d);
         pen.setStrokeWidth(2.0);
         if(turtle.isCleared()){
-            pen.getPoints().clear();
+            for (Object o : myLines) {
+                Polyline p = (Polyline) o;
+                p.getPoints().clear();
+            }
         }
+        myTurtleHabitat.getChildren().add(pen);
     }
 }
