597fe85fd35c4cb5b7e16c53a95ba898df643615
QIAOYI FANG <QF30@duke.edu>
2020-02-25 20:34:16 -0500

    added more to variable_panel

--- src/slogo/variable_panels/VariablesTabPane.java
+++ src/slogo/variable_panels/VariablesTabPane.java
@@ -1,6 +1,18 @@
 package slogo.variable_panels;
 
+import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
 
 public class VariablesTabPane extends TabPane {
+    private final static String[] tabNames = {"Command history", "Defined Methods", "Variables"};
+
+    public VariablesTabPane(double width, double height){
+        setPrefSize(width, height);
+    }
+
+    private void initializeTabPane(){
+        for (String tabName: tabNames){
+            Tab p
+        }
+    }
 }
