231a1fa68aa532caed7b6a58471bcf9715be71b9
Maverick Chung <mc608@duke.edu>
2020-02-26 12:09:25 -0500

    removed debug printstacktrace

--- src/slogo/compiler/Compiler.java
+++ src/slogo/compiler/Compiler.java
@@ -93,7 +93,6 @@ public class Compiler {
       return "" + comm.execute();
     } catch (CompilerException e) {
       //throw e;
-      e.printStackTrace();
       return e.toString();
     }
   }
